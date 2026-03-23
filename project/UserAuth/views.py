d0636ec6afcac2538689e354e42500a88637783d
MrIncxyl <tsktechnical7@gmail.com>
2026-03-23 13:30:59 +0530

    Message Implementation

--- project/UserAuth/views.py
+++ project/UserAuth/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -11,6 +11,10 @@ def Register(request):
         username = request.POST.get('username')
         email = request.POST.get('useremail')
         password = request.POST.get('password')
+        
+        if not username or not email or not password:
+            messages.error(request,"All fields are required")
+            return redirect('register') 
         
         alreadyExist = User.objects.filter(username=username).exists()
         
