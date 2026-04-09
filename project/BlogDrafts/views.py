a6eb564ecf28e4654e9a6bea62ca924371aab0cd
MrIncxyl <tsktechnical7@gmail.com>
2026-04-09 16:18:36 +0530

    Delete category Add Category

--- project/BlogDrafts/views.py
+++ project/BlogDrafts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Category, Blog
 
 # Create your views here.
 
@@ -9,4 +11,29 @@ def Drafts(request):
 
 @login_required(login_url='login')
 def Create(request):
-    return render(request,'create.html')
+    
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        # handle add category
+        if 'add_category' in request.POST:
+            category = request.POST.get('category')
+            
+            if(category):
+                Category.objects.get_or_create(name=category)
+                messages.success(request,f'{category} Category Added Successfully.')
+            else:
+                messages.error(request,'Category Field is Required.')    
+                return redirect('create')
+                         
+        # handle create blog
+        if 'add_blog' in request.POST:
+            pass    
+            
+        if  'delete_category' in request.POST:
+            id = request.POST.get('delete_category') 
+            dl_cat =  Category.objects.get(id=id)
+            dl_cat.delete()
+            messages.success(request,f"{dl_cat}  Deleted Successfully.")
+            return redirect('create')
+             
+    return render(request,'create.html',{'data':categories})
